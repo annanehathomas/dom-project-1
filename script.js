44fee3cf4986efa08012714473eba025ebdc733c
annanehathomas <annanehathomas@gmail.com>
2025-02-22 17:42:28 +0530

    added files for project-1

---
+++ script.js
@@ -0,0 +1,10 @@
+let mydiv = document.getElementById("mydiv");
+
+mydiv.style.backgroundColor ="yellow";
+mydiv.style.margin = "20px";
+mydiv.style.padding ="10px";
+mydiv.style.fontSize = "18px";
+mydiv.style.fontWeight = "bold";
+mydiv.style.height = "200px";
+mydiv.style.width = "300px";
+mydiv.style.color ="blue";
